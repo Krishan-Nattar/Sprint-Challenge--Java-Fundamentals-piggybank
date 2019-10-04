3108d27a3ba081a5bc178818c3b3e445c70bc4d1
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-04 08:57:30 -0700

    working on deducting quarters

--- piggybank/src/piggyBank/Main.java
+++ piggybank/src/piggyBank/Main.java
@@ -44,6 +44,27 @@ public class Main {
                 }
             }
 
+            while(amount >= 0.25)
+            {
+                int count = 0;
+                for(Coin c: piggyBank)
+                {
+                    if(c instanceof Quarter && c.getTotal() >=1 && amount > 1)
+                    {
+                        c.removeCoin(1);
+                        amount -= 0.25;
+                        count++;
+                        break; //start while loop over to check if amount is still >= 1
+                    }
+                }
+                if(count==0) //If no coins of this denomination were found.. Break out of while loop and continue to next denomination
+                {
+                    break;
+                } else { //Otherwise, reset count and start while loop over
+                    count = 0;
+                }
+            }
+
 
 
 
