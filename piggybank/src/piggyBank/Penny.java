6f99a8a5009238137544c9a7bc6eea5fa41c788f
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-04 08:28:46 -0700

    Finished pennies

---
+++ piggybank/src/piggyBank/Penny.java
@@ -0,0 +1,41 @@
+package piggyBank;
+
+public class Quarter implements Coin {
+    private int count;
+    private double value; //Needs to be in currency format?
+
+    public Quarter() //If you initialize without a count....
+    {
+        //Should default to one coin being added
+        this.count = 1;
+        this.value = 0.25;
+    }
+
+    public Quarter(int count) //If you initialize without a count....
+    {
+        //Should default to one coin being added
+        this.count = count;
+        this.value = 0.25;
+    }
+
+    @Override
+    public void removeCoin()
+    {
+        //finish this later
+    }
+
+
+    @Override
+    public String getCount() {
+        if (this.count ==1) {
+            return this.count + " Quarter";
+        } else {
+            return this.count + " Quarters"; //Plurals
+        }
+    }
+
+    @Override
+    public Double getTotal() {
+        return this.count * this.value;
+    }
+}
