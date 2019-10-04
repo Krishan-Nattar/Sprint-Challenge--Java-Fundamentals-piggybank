235e6debb58978c2a5ada521d88c0d61d2a431cc
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-04 09:02:32 -0700

    dimes working

--- piggybank/src/piggyBank/Main.java
+++ piggybank/src/piggyBank/Main.java
@@ -65,6 +65,27 @@ public class Main {
                 }
             }
 
+            while(amount >= 0.1)
+            {
+                int count = 0;
+                for(Coin c: piggyBank)
+                {
+                    if(c instanceof Dime && c.getTotal() >=0.1 && amount > 0.1)
+                    {
+                        c.removeCoin(1);
+                        amount -= 0.1;
+                        count++;
+                        break; //start while loop over to check if amount is still >= 1
+                    }
+                }
+                if(count==0) //If no coins of this denomination were found.. Break out of while loop and continue to next denomination
+                {
+                    break;
+                } else { //Otherwise, reset count and start while loop over
+                    count = 0;
+                }
+            }
+
 
 
 
