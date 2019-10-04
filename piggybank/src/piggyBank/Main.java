bdf7640677e250d50903594e75d8697acb78ed45
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-04 08:08:00 -0700

    decimalsformatting and last sysout copied from readme

--- piggybank/src/piggyBank/Main.java
+++ piggybank/src/piggyBank/Main.java
@@ -1,8 +1,17 @@
 package piggyBank;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Currency;
+
 public class Main {
 
+    static ArrayList<Currency> piggyBank = new ArrayList<>();
+
     public static void main(String[] args) {
+        DecimalFormat fp = new DecimalFormat("$###,###.00");
 	// write your code here
+        int myValue = 0;
+        System.out.println("The piggy bank holds " + fp.format(myValue));
     }
 }
