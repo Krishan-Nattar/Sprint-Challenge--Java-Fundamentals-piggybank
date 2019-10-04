fa6ea1dec06dd363f95a4442952e60deb88ed25f
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-04 08:10:20 -0700

    copying add coins from readme

--- piggybank/src/piggyBank/Main.java
+++ piggybank/src/piggyBank/Main.java
@@ -2,15 +2,25 @@ package piggyBank;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Currency;
 
 public class Main {
 
-    static ArrayList<Currency> piggyBank = new ArrayList<>();
+    static ArrayList<Coin> piggyBank = new ArrayList<Coin>(); //Dollars can be coins too...
+
 
     public static void main(String[] args) {
+        System.out.println("*** STARTING PROGRAM ***");
         DecimalFormat fp = new DecimalFormat("$###,###.00");
-	// write your code here
+
+        piggyBank.add(new Quarter());
+        piggyBank.add(new Dime());
+        piggyBank.add(new Dollar(5));
+        piggyBank.add(new Nickel(3));
+        piggyBank.add(new Dime(7));
+        piggyBank.add(new Dollar());
+        piggyBank.add(new Penny(10));
+
+        
         int myValue = 0;
         System.out.println("The piggy bank holds " + fp.format(myValue));
     }
