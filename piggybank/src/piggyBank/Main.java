2eee38d4ae5840d03f6ac4155c8d2d4107d51dfa
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-04 08:29:55 -0700

    mvp complete

--- piggybank/src/piggyBank/Main.java
+++ piggybank/src/piggyBank/Main.java
@@ -9,16 +9,18 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("*** STARTING PROGRAM ***");
+        System.out.println();
+        System.out.println("*** PIGGYBANK SPRINT ***");
+        System.out.println();
         DecimalFormat fp = new DecimalFormat("$###,###.00");
 
         piggyBank.add(new Quarter());
         piggyBank.add(new Dime());
         piggyBank.add(new Dollar(5));
         piggyBank.add(new Nickel(3));
-//        piggyBank.add(new Dime(7));
-//        piggyBank.add(new Dollar());
-//        piggyBank.add(new Penny(10));
+        piggyBank.add(new Dime(7));
+        piggyBank.add(new Dollar());
+        piggyBank.add(new Penny(10));
 
         double myValue = 0;
 
@@ -29,8 +31,8 @@ public class Main {
         }
 
         System.out.println();
-        System.out.println("*************************");
+        System.out.println("****************************");
         System.out.println("The piggy bank holds " + fp.format(myValue));
-        System.out.println("*************************");
+        System.out.println("****************************");
     }
 }
--- piggybank/src/piggyBank/Penny.java
+++ piggybank/src/piggyBank/Penny.java
@@ -1,21 +1,21 @@
 package piggyBank;
 
-public class Quarter implements Coin {
+public class Penny implements Coin {
     private int count;
     private double value; //Needs to be in currency format?
 
-    public Quarter() //If you initialize without a count....
+    public Penny() //If you initialize without a count....
     {
         //Should default to one coin being added
         this.count = 1;
-        this.value = 0.25;
+        this.value = 0.01;
     }
 
-    public Quarter(int count) //If you initialize without a count....
+    public Penny(int count) //If you initialize without a count....
     {
         //Should default to one coin being added
         this.count = count;
-        this.value = 0.25;
+        this.value = 0.01;
     }
 
     @Override
@@ -28,9 +28,9 @@ public class Quarter implements Coin {
     @Override
     public String getCount() {
         if (this.count ==1) {
-            return this.count + " Quarter";
+            return this.count + " Penny";
         } else {
-            return this.count + " Quarters"; //Plurals
+            return this.count + " Pennies"; //Plurals
         }
     }
 
